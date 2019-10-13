4f6bfa2546975cfc55749db9a66932c57e4c4bc2
Samuel Padilla C <35386211+SamuelPadillaC@users.noreply.github.com>
2019-10-13 12:42:28 -0500

    Update jsonFormatter.py

--- jsonFormatter.py
+++ jsonFormatter.py
@@ -2,44 +2,36 @@ import json
 
 def formatForML(input_data_path, output_data_path):
 
-#dictionary to be saved in JSON at the end
-	mydict = {}
+	f = open(input_data_path, "r")
 
-	#change the jason file name - data_path
-	#data_path = "dummy.json"
-	#output_file = "output1.json"
+	for line in f:
+		if line != '\n':
+			data = json.loads(line)
 
+	#dictionary to be saved in JSON at the end
+		mydict = {}
 
+		#iterate over attributes
+		for element in data:
+			if "id_str" in element and len(element) == 6:
+				mydict["id"] = data["id_str"]
 
-	with open(input_data_path, "r") as data_file:
-		data = json.load(data_file)
+			if "text" in element and len(element) == 4:
+				mydict["text"] = data["text"]
 
-	#iterate over attributes
-	for element in data:
-		if 'id' in element:
-		#save the id value to dictionary
-			mydict["id"] = data['id']
+			if "user" in element and len(element) == 4:
+				#print(element)
+				for a in data[element]:
+					if "location" in a:
+						mydict["loc"] = data[element][a]
 
-		if "text" in element:
-			mydict["text"] = data["text"]
+			if "lang" in element and len(element) == 4:
+					mydict["lang"] = data["lang"]
 
-		if "user" in element:
-			for ind, a in enumerate(data[element]):
-				if "location" in a:
-					
-					mydict["loc"] = data[element][a]
-
-		if "quoted_status" in element:
-			for b in data[element]:
-				if "lang" in b:
-					mydict["lang"] = data[element][b]
-
-		#open the output file in append mode and write json
-	with open(output_data_path, 'a') as f:
-		json.dump(mydict, f)
-
-	resultingJSON = json.loads(output_data_path)
-	return resultingJSON
+			#open the output file in append mode and write json
+		with open(output_data_path, 'a') as f:
+			json.dump(mydict, f)
 
 	
-
+	resultingJSON = json.loads(output_data_path)
+	return resultingJSON
