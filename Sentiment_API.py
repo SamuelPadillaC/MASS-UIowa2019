c84286f30d0b007247a3b369bb6c06811930da07
Samuel Padilla C <35386211+SamuelPadillaC@users.noreply.github.com>
2019-10-13 12:43:18 -0500

    Update Sentiment_API.py

--- Sentiment_API.py
+++ Sentiment_API.py
@@ -2,32 +2,61 @@ import requests
 import json
 #Azure credentials is in the same folder
 from Azure_Credentials import *
+from jsonFormatter import *
 
-# define the data to be analyzed
-payload = {
-  "documents": [
-    {
-      "language": "en",
-      "id": "1",
-      "text": "Hello world. I fucking hate my fucking life fuck this shit."
-    },
-    {
-      "language": "fr",
-      "id": "2",
-      "text": "Bonjour tout le monde"
-    },
-    {
-      "language": "es",
-      "id": "3",
-      "text": "La carretera estaba atascada. Había mucho tráfico el día de ayer."
-    }
-  ]
-}
 
-# sending POST request and saving the response as response object 
-r = requests.post(url=URL, headers=headers, json=payload)
+def Calculate_Sentiment (json_file, output_file):
+  payload = {'documents': []}
+  location = {'documents': []}
+  
+  f = open(json_file, "r")
 
-# extracting data in json format 
-data = r.json()
+  for data_line in f:
+    
+    if data_line != '\n':
+      data = json.loads(data_line)
 
-print (data)
+      dummy_dict = {}
+
+      for element in data:
+        if "id" in element:
+            dummy_dict["id"] = data[element]
+
+        if "text" in element:
+            dummy_dict["text"] = data[element]
+
+        if "lang" in element:
+            dummy_dict["lang"] = data[element]
+
+        if "loc" in element:
+            location["documents"].append({"loc": data[element]})
+
+      payload["documents"].append(dummy_dict)
+
+  # sending POST request and saving the response as response object 
+  r = requests.post(url=URL, headers=headers, json=payload)
+
+  # extracting data in json format 
+  data_received = r.json()
+
+  # opening the output_file
+  O = open(output_file, 'a')  
+
+  for received in data_received["documents"]:
+      output_dummy_dic = {}
+      
+      if "id" in received:
+        output_dummy_dic["id"] = received["id"]
+
+      if "score" in received:
+        output_dummy_dic["positivity"] = received["score"]
+
+      output_dummy_dic["text"] = payload["documents"][data_received["documents"].index(received)]["text"]
+      output_dummy_dic["lang"] = payload["documents"][data_received["documents"].index(received)]["lang"]
+      output_dummy_dic["loc"] = location["documents"][data_received["documents"].index(received)]["loc"]
+
+      json.dump(output_dummy_dic, O)
+      json.dump('\n', O)
+
+
+  return output_file
